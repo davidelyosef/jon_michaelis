3ad3448537eea9de3122eadae44471e7f41e1873
Daphne Levy <87daphnelevy@gmail.com>
2020-07-15 18:21:21 +0300

    add modal

--- src/app/paintings/paintings.component.ts
+++ src/app/paintings/paintings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { openModal, closeModal, previousImage, nextImage, onKeyUp } from 'src/app/global';
 import { Painting } from 'src/modals/painting';
 
 @Component({
@@ -10,6 +11,16 @@ export class PaintingsComponent implements OnInit {
   public paintings: Painting[] = [];
   public splash: Painting[] = [];
   public tvCartoons: Painting[] = [];
+  public painting: any = {};
+  @ViewChild('artworkModal', { static: false }) artworkModal: ElementRef;
+  // modal states
+
+  public openModal = openModal;
+  public closeModal = closeModal;
+  public previousImage = previousImage;
+  public nextImage = nextImage;
+  public onKeyUp = onKeyUp;
+
 
   constructor() { }
 
@@ -17,6 +28,7 @@ export class PaintingsComponent implements OnInit {
     fetch('assets/json/paintings.json').then(data => data.json())
       .then(paintings =>
         paintings.map(painting => {
+          this.paintings = paintings;
           if (painting.category === 'splash') this.splash.push(painting);
           else if (painting.category === 'tv cartoons') this.tvCartoons.push(painting);
           else this.paintings.push(painting);
