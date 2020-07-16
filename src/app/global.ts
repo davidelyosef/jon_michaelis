86d0b25b49fc241a17957cf3e119d8cfd9816d1a
David el Yosef <davidyf96@gmail.com>
2020-07-16 12:47:23 +0300

    update

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,51 +1,41 @@
-export const openModal = function(id) {
-    
-    this.paintings.map(painting => {
-        if (painting.id === id) {
-            this.painting = painting;
-            this.index = this.paintings.indexOf(painting)
-            this.maxLength = this.paintings.length - 1;
-        }
-    })
-    this.artworkModal.nativeElement.style.display = 'block';
- 
+export const openModal = function(paint) {
+  this.painting = paint;
+  this.index = this.paintings.indexOf(paint);
+  this.maxLength = this.paintings.length - 1;
+  this.artworkModal.nativeElement.style.display = 'block';
 }
 
 export const closeModal = function() {
- 
-    this.artworkModal.nativeElement.style.display = 'none';
+  this.artworkModal.nativeElement.style.display = 'none';
 }
 
 export const previousImage = function() {
- 
-    if (this.index == 0) this.index = this.paintings.length;
-    this.painting = this.paintings[this.index - 1];
-    this.index--;
+  this.index--;
+  if (this.index == 0) this.index = this.paintings.length;
+  this.painting = this.paintings[this.index - 1];
 }
 
 export const nextImage = function() {
-
-    if (this.index + 1 > this.maxLength) this.index = -1;
-    this.painting = this.paintings[this.index + 1];
-    this.index++;
+  this.index++;
+  if (this.index > this.maxLength) this.index = 0;
+  this.painting = this.paintings[this.index];
 }
 
-export const onKeyUp = function(ev:KeyboardEvent) {
-
-    if( ev.key === "ArrowLeft"){
-      if (this.index == 0) this.index = this.paintings.length;
-      this.painting = this.paintings[this.index - 1];
-      this.index--;
-    }
-
-    else if(ev.key === "ArrowRight"){
-      if (this.index + 1 > this.maxLength) this.index = -1;
-      this.painting = this.paintings[this.index + 1];
-      this.index++;
-    }
-
-    else if(ev.key === "Escape"){
-        this.artworkModal.nativeElement.style.display = 'none';
-      }
-    console.log(`The user just pressed ${ev.key}!`);
+export const onKeyUp = function (ev: KeyboardEvent) {
+  if (ev.key === "ArrowLeft") {
+    this.index--;
+    if (this.index == 0) this.index = this.paintings.length - 1;
+    this.painting = this.paintings[this.index];
   }
+
+  else if (ev.key === "ArrowRight") {
+    this.index++;
+    if (this.index + 1 > this.maxLength) this.index = -1;
+    this.painting = this.paintings[this.index + 1];
+  }
+
+  else if (ev.key === "Escape") {
+    this.artworkModal.nativeElement.style.display = 'none';
+  }
+  console.log(`The user just pressed ${ev.key}!`);
+}
