1f4aefa1015727da6d17c6c24fda9d82a40b7862
Daphne Levy <87daphnelevy@gmail.com>
2020-07-19 18:04:31 +0300

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBOk_gmQJi56dZWRtL7x5S-oTqvhOiYhrE",
+  authDomain: "jonmichaelis-df88d.firebaseapp.com",
+  databaseURL: "https://jonmichaelis-df88d.firebaseio.com",
+  projectId: "jonmichaelis-df88d",
+  storageBucket: "jonmichaelis-df88d.appspot.com",
+  messagingSenderId: "681679918505",
+  appId: "1:681679918505:web:fc9a0257ae17b6b950e0b8"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false, 
+  production: true, 
   firebase : {
     apiKey: "AIzaSyBOk_gmQJi56dZWRtL7x5S-oTqvhOiYhrE",
   authDomain: "jonmichaelis-df88d.firebaseapp.com",
